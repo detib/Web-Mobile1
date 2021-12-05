6e150dc5f636fbbb27cbc86a3b309e857614aa8a
Deti Baholli <detibaholli@gmail.com>
2021-12-05 22:41:10 +0100

    navbar2

---
+++ js/index.js
@@ -0,0 +1,9 @@
+function showMenu() {
+    document.getElementById("mobileMenu").style.width = "20%";
+    // document.getElementById("mobileMenu").style.display = "block";
+}
+
+function closeMenu() {
+    document.getElementById("mobileMenu").style.width = "0px";
+    // document.getElementById("mobileMenu").style.display = "none";
+}
