5e3c6a80dfb194beed3edd9f6e288debff681f65
Deti Baholli <detibaholli@gmail.com>
2021-12-06 16:32:25 +0100

    homepage

--- js/index.js
+++ js/index.js
@@ -1,7 +1,25 @@
+import '../css'
+
 function showMenu() {
     document.getElementById("mobileMenu").style.width = "17%";
 }
 
 function closeMenu() {
     document.getElementById("mobileMenu").style.width = "0px";
+}
+
+var scrollToTopButton = document.getElementById("scrollToTopButton");
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        scrollToTopButton.style.display = "block";
+    } else {
+        scrollToTopButton.style.display = "none";
+    }
+}
+
+function scroll() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
 }
