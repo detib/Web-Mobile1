3d2a2b1f0693f8447fd2b57c176eb9ef935a65fd
Deti Baholli <detibaholli@gmail.com>
2021-12-16 00:53:46 +0100

    123

--- js/opacity.js
+++ js/opacity.js
@@ -3,9 +3,7 @@
 // this function changes the navbar background-color the same way the function above changes the class
 // this function changes the navbar background-color to rgba(0,0,0,0.7) when the user scrolls 200 units down
 // and when lower than 200 units then the background-color is changed to rgba(0,0,0,0)
-// window.onscroll = function() {
-//     navbarOpacity();
-// }
+
 window.addEventListener('scroll', navbarOpacity);
 var navbar = document.getElementById("navbar");
 function navbarOpacity() {
