9d829c8bcff97f393daa3403bdf17fe81e62b2ab
Deti Baholli <detibaholli@gmail.com>
2021-12-15 22:24:53 +0100

    merri qito Cars e kom ndreq edhe e bona ni js file

--- js/index.js
+++ js/index.js
@@ -11,7 +11,7 @@ function closeMenu() {
 }
 
 //window.onscroll creates a function that calls the functions scrollFunction(), and navbarOpacity() every time the user scrolls
-window.onscroll = function () { scrollFunction(); navbarOpacity() };
+window.onscroll = function () { scrollFunction()};
 // ==================================================================
 // ==================================================================
 // ==================================================================
@@ -34,20 +34,6 @@ function scrollFunction() {
 }
 // ==================================================================
 // ==================================================================
-
-// ==================================================================
-// ==================================================================
-// this function changes the navbar background-color the same way the function above changes the class
-// this function changes the navbar background-color to rgba(0,0,0,0.7) when the user scrolls 200 units down
-// and when lower than 200 units then the background-color is changed to rgba(0,0,0,0)
-var navbar = document.getElementById("navbar");
-function navbarOpacity() {
-    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
-        navbar.style.backgroundColor = "rgba(0,0,0,0.7)"
-    } else {
-        navbar.style.backgroundColor = "rgba(0,0,0,0)"
-    }
-}
 
 
 // ==================================================================
